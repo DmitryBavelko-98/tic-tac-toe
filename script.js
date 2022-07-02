d51757c063671dbf1be3febdd55842c9aaa5a4f8
DmitryBavelko-98 <bavelko90@gmail.com>
2022-07-02 17:40:43 +0300

    Add gameboard and player objects

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const cells = document.querySelectorAll('.gameboard__cell');
+
+const gameBoard = (() => {
+    const game = [];
+    let turn = 1;
+    
+    const round = (e, mark) => {
+        e.target.innerHTML = mark;
+        turn++;
+    }
+
+    return {
+        round,
+        turn
+    }
+})();
+
+const Player = (mark) => {
+    const move = () => {
+        cells.forEach(cell => {
+            cell.addEventListener('click', (e) => {
+                console.log(gameBoard.turn);
+                gameBoard.round(e, mark);
+            })
+        })
+    }   
+    return {move}
+}
+
+const playerOne = Player('x');
+const playerTwo = Player('o');
+
+
+if (gameBoard.turn % 2 !== 0) {
+    playerOne.move();
+} else playerTwo.move();
+
+
+
