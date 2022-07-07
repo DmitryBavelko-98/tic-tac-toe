e1df5d6fa83d9642335dc5db32a8778200bac0fd
DmitryBavelko-98 <bavelko90@gmail.com>
2022-07-07 16:45:30 +0300

    Change structure and add styles

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const board = document.querySelector('.gameboard__cells')
 const cells = document.querySelectorAll('.gameboard__cell');
 
 const gameBoard = (() => {
@@ -7,35 +8,38 @@ const gameBoard = (() => {
     let turn = 1;
     
     const round = (e, mark) => {
-        e.target.innerHTML = mark;
-        turn++;
+        const target = e.target;
+        target.innerHTML = mark;
+        game.push(target.innerHTML);
     }
 
     return {
         round,
-        turn
+        game
     }
 })();
 
 const Player = (mark) => {
-    const move = () => {
-        cells.forEach(cell => {
-            cell.addEventListener('click', (e) => {
-                console.log(gameBoard.turn);
-                gameBoard.round(e, mark);
-            })
-        })
-    }   
+    const move = (e) => {
+        if (e.target.innerHTML !== '') return;
+        gameBoard.round(e, mark);
+    }
     return {move}
 }
 
 const playerOne = Player('x');
 const playerTwo = Player('o');
 
+function declareWinner () {
+    
+}
 
-if (gameBoard.turn % 2 !== 0) {
-    playerOne.move();
-} else playerTwo.move();
-
-
-
+for (let i = 0; i < cells.length; i++) {
+    cells[i].addEventListener('click', (e) => {
+        if (gameBoard.game.length % 2 === 0) {
+            playerOne.move(e);
+        } else {
+            playerTwo.move(e);
+        }   
+    })  
+}
