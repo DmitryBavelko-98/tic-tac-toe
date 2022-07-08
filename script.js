9137faca3fb49349d1d6ac522a4ae3f3c6b2c3ae
DmitryBavelko-98 <bavelko90@gmail.com>
2022-07-08 15:21:22 +0300

    Add winner check

--- script.js
+++ script.js
@@ -4,24 +4,52 @@ const board = document.querySelector('.gameboard__cells')
 const cells = document.querySelectorAll('.gameboard__cell');
 
 const gameBoard = (() => {
-    const game = [];
+    const game = ['', '', '', '', '', '', '', '', ''];
     let turn = 1;
+
+    const conditions = [
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7 ,8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
     
     const round = (e, mark) => {
         const target = e.target;
+        const order = +target.dataset.order;
         target.innerHTML = mark;
-        game.push(target.innerHTML);
+        game[order - 1] = mark;
+        conditions.forEach(checkWinner);
+    }
+
+    function checkWinner (cells) {
+        if (game[cells[0]] === 'x' && game[cells[1]] === 'x' && game[cells[2]] === 'x') {
+            console.log("Player one wins!");
+            gameOver();
+        } else if (game[cells[0]] === 'o' && game[cells[1]] === 'o' && game[cells[2]] === 'o') {
+            console.log("Player two wins!");
+            gameOver();
+        }
+    }
+    
+    function gameOver() {
+        cells.forEach(cell => {
+            cell.removeEventListener('click', markField)
+        })
     }
 
     return {
         round,
-        game
+        turn
     }
 })();
 
 const Player = (mark) => {
     const move = (e) => {
-        if (e.target.innerHTML !== '') return;
         gameBoard.round(e, mark);
     }
     return {move}
@@ -30,12 +58,17 @@ const Player = (mark) => {
 const playerOne = Player('x');
 const playerTwo = Player('o');
 
+function markField (e) {
+    if (e.target.innerHTML !== '') return
+    if (gameBoard.turn % 2 !== 0) {
+        playerOne.move(e);
+        gameBoard.turn++;
+    } else {
+        playerTwo.move(e);
+        gameBoard.turn++
+    }   
+}
+
 for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', (e) => {
-        if (gameBoard.game.length % 2 === 0) {
-            playerOne.move(e);
-        } else {
-            playerTwo.move(e);
-        }   
-    })  
+    cells[i].addEventListener('click', markField)
 }
