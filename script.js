5814157fab5be46d6ba18cfe182c929f401a74f0
DmitryBavelko-98 <bavelko90@gmail.com>
2022-07-08 20:11:51 +0300

    Add reset button and refactor code

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const board = document.querySelector('.gameboard__cells')
+const board = document.querySelector('.gameboard__cells');
 const cells = document.querySelectorAll('.gameboard__cell');
+const status = document.querySelector('.status__message');
+const reset = document.querySelector('.reset-btn');
 
 const gameBoard = (() => {
-    const game = ['', '', '', '', '', '', '', '', ''];
+    let game = ['', '', '', '', '', '', '', '', ''];
     let turn = 1;
 
     const conditions = [
@@ -21,54 +23,75 @@ const gameBoard = (() => {
     const round = (e, mark) => {
         const target = e.target;
         const order = +target.dataset.order;
-        target.innerHTML = mark;
+        target.textContent = mark;
         game[order - 1] = mark;
-        conditions.forEach(checkWinner);
+        for (let condition of conditions) {
+            checkWinner(condition);
+        }
     }
 
     function checkWinner (cells) {
         if (game[cells[0]] === 'x' && game[cells[1]] === 'x' && game[cells[2]] === 'x') {
-            console.log("Player one wins!");
+            status.textContent = "Player X wins!";
             gameOver();
         } else if (game[cells[0]] === 'o' && game[cells[1]] === 'o' && game[cells[2]] === 'o') {
-            console.log("Player two wins!");
+            status.textContent = "Player O wins!";
             gameOver();
-        }
+        } else if (game.every(item => item !== '')) status.textContent = "It's a tie!";
     }
     
     function gameOver() {
-        cells.forEach(cell => {
-            cell.removeEventListener('click', markField)
-        })
+        board.removeEventListener('click', playGame);
+    }
+
+    function resetField() {
+        game = game.map(item => item = '');
+        cells.forEach(cell => cell.textContent = '');
+        status.textContent = 'Player X';
+        turn = 1;
+    }
+
+    function changeTurn() {
+        turn += 1;
+    }
+
+    function playGame (e) {
+        if (e.target.innerHTML !== '') return
+        if (turn % 2 !== 0) {
+            playerTwo.showTurn();
+            playerOne.move(e);
+        } else {
+            playerOne.showTurn();
+            playerTwo.move(e);
+        }   
     }
 
     return {
         round,
-        turn
+        changeTurn,
+        resetField,
+        turn,
+        playGame
     }
 })();
 
-const Player = (mark) => {
+const Player = (name, mark) => {
+    const showTurn = () => {
+        status.textContent = name;
+    };
     const move = (e) => {
         gameBoard.round(e, mark);
+        gameBoard.changeTurn();
     }
-    return {move}
+    return {showTurn, move}
 }
 
-const playerOne = Player('x');
-const playerTwo = Player('o');
+const playerOne = Player('Player X', 'x');
+const playerTwo = Player('Player O', 'o');
 
-function markField (e) {
-    if (e.target.innerHTML !== '') return
-    if (gameBoard.turn % 2 !== 0) {
-        playerOne.move(e);
-        gameBoard.turn++;
-    } else {
-        playerTwo.move(e);
-        gameBoard.turn++
-    }   
-}
+board.addEventListener('click', gameBoard.playGame)
 
-for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener('click', markField)
-}
+reset.addEventListener('click', () => {
+    gameBoard.resetField();
+    board.addEventListener('click', gameBoard.playGame);
+});
